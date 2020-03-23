39928f0d3ca6f21a3742c60769700672e401d8db
mihan <m.menuka@gmail.com>
2020-03-24 00:08:52 +0530

    updated country flag, last updated date and user refresh button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { noop, timer } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'covid19live';
+
+export class AppComponent implements OnInit {
+
+  globalTotal: any = {};
+
+  countryData: any;
+
+  dateAvailability = true;
+
+  displayedColumns: string[] = ['countryInfo', 'country', 'cases', 'todayCases', 'deaths', 'todayDeaths', 'recovered', 'critical'];
+
+  dataSource;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  getData() {
+    this.getCountries();
+    this.getAll();
+  }
+
+  getAll() {
+    this.http.get('all').subscribe(
+      (data: any) => {
+        this.globalTotal = data;
+      },
+      noop,
+      noop
+    );
+  }
+
+  getCountries() {
+    this.http.get('countries').subscribe(
+      (data: any) => {
+        this.countryData = data;
+        this.dateAvailability = true;
+        this.dataSource = new MatTableDataSource(this.countryData);
+      },
+      () => {
+        this.dateAvailability = false;
+      },
+      noop
+    );
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatInputModule} from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatInputModule,
+    MatTableModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
